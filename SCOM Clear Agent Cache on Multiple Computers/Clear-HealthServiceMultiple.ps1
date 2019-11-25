5775514a8fa9139dc9bb4c04efe32d1031c81dfb
Leon Laude <44682947+LeonLaude@users.noreply.github.com>
2019-11-25 14:15:36 +0100

    Update Clear-HealthServiceMultiple.ps1

--- SCOM Clear Agent Cache on Multiple Computers/Clear-HealthServiceMultiple.ps1
+++ SCOM Clear Agent Cache on Multiple Computers/Clear-HealthServiceMultiple.ps1
@@ -1,14 +1,18 @@
-﻿# --------------------------------
+# --------------------------------
 # | Author: Leon Laude           |
 # | Email:  leon.laude@gmail.com |
 # --------------------------------
 #
 # This is a simple script for clearing the SCOM agent cache on multiple agent-managed computers that are listed in a text file.
 #
+Write-Host "Enter the full path of your text file containing the server list." -ForegroundColor Cyan
+$FilePath = Read-Host "Path"
 
-$Servers = Get-Content "C:\Temp\Servers.txt"
+Write-Host "1. Getting the list of computers from the text file:"$FilePath`n -ForegroundColor Cyan
 
-Write-Host "1. Stopping the Monitoring Agent service on the computers...`n" -ForegroundColor Cyan
+$Servers = Get-Content $FilePath
+
+Write-Host "2. Stopping the Monitoring Agent service on the computers...`n" -ForegroundColor Cyan
 
 ForEach ($Server in $Servers) 
 {  
@@ -19,7 +23,7 @@ Get-Service -ComputerName $Server -Name HealthService | Stop-Service
 
 Start-Sleep -s 3
 
-Write-Host "2. Renaming the existing 'Health Service State' folder to 'Health Service State Old' `n" -ForegroundColor Cyan
+Write-Host "3. Renaming the existing 'Health Service State' folder to 'Health Service State Old' `n" -ForegroundColor Cyan
 
 ForEach ($Server in $Servers) 
 {  
@@ -30,7 +34,7 @@ Rename-Item -Path "\\$Server\C$\Program Files\Microsoft Monitoring Agent\Agent\H
 
 Start-Sleep -s 3
 
-Write-Host "3. Starting the Monitoring Agent service...`n" -ForegroundColor Cyan
+Write-Host "4. Starting the Monitoring Agent service...`n" -ForegroundColor Cyan
 
 ForEach ($Server in $Servers) 
 {  
@@ -41,7 +45,7 @@ Get-Service -ComputerName $Server -Name HealthService | Start-Service
 
 Start-Sleep -s 3
 
-Write-Host "4. Removing the 'Health Service State Old' folder." -ForegroundColor Cyan
+Write-Host "5. Removing the 'Health Service State Old' folder." -ForegroundColor Cyan
 
 ForEach ($Server in $Servers) 
 {  
@@ -50,4 +54,4 @@ Remove-Item -Path "\\$Server\C$\Program Files\Microsoft Monitoring Agent\Agent\H
 
 }
 
-Write-Host "`n5. Done!" -ForegroundColor Cyan
+Write-Host "`n6. Done!" -ForegroundColor Cyan
