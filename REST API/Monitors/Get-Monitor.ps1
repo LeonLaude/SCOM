364977e1874df9f37da265c74f6181e04ddc4c11
Leon Laude <44682947+LeonLaude@users.noreply.github.com>
2020-07-09 13:49:08 +0200

    Add files via upload

---
+++ REST API/Monitors/Get-Monitor.ps1
@@ -0,0 +1,27 @@
+﻿# Set the Header and the Body
+$SCOMHeaders = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$SCOMHeaders.Add('Content-Type','application/json; charset=utf-8')
+$BodyRaw = "Windows"
+$Bytes = [System.Text.Encoding]::UTF8.GetBytes($bodyraw)
+$EncodedText =[Convert]::ToBase64String($Bytes)
+$JSONBody = $EncodedText | ConvertTo-Json
+
+# The SCOM REST API authentication URL
+$UriBase = 'http://<Your SCOM MS>/OperationsManager/authenticate'
+
+# Authentication
+$Authentication = Invoke-RestMethod -Method Post -Uri $UriBase -Headers $SCOMHeaders -Body $JSONBody -UseDefaultCredentials -SessionVariable WebSession
+
+# Criteria: Enter the displayname of the SCOM monitor
+$Criteria = "DisplayName LIKE '%SQL Server%'"
+
+# Convert our criteria to JSON format
+$JSONBody = $Criteria | ConvertTo-Json
+
+$Response = Invoke-WebRequest -Uri 'http://<Your SCOM MS>/OperationsManager/data/class/monitors' -Method Post -Body $JSONBody -WebSession $WebSession
+
+# Convert our response from JSON format to a custom object or hash table
+$Monitors = ConvertFrom-Json -InputObject $Response.Content
+
+# Print out the monitor results
+$Monitors.rows
