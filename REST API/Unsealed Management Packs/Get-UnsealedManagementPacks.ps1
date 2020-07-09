0aa0db3403a07fc55abcada68895d11201fa0bf5
Leon Laude <44682947+LeonLaude@users.noreply.github.com>
2020-07-09 13:58:19 +0200

    Add files via upload

---
+++ REST API/Unsealed Management Packs/Get-UnsealedManagementPacks.ps1
@@ -0,0 +1,21 @@
+﻿# Set the Header and the Body
+$SCOMHeaders = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
+$SCOMHeaders.Add('Content-Type','application/json; charset=utf-8')
+$BodyRaw = "Windows"
+$Bytes = [System.Text.Encoding]::UTF8.GetBytes($bodyraw)
+$EncodedText =[Convert]::ToBase64String($Bytes)
+$JSONBody = $EncodedText | ConvertTo-Json
+
+# The SCOM REST API authentication URL
+$UriBase = 'http://<Your SCOM MS>/OperationsManager/authenticate'
+
+# Authentication
+$Authentication = Invoke-RestMethod -Method Post -Uri $UriBase -Headers $SCOMHeaders -Body $JSONBody -UseDefaultCredentials -SessionVariable WebSession
+
+$Response = Invoke-WebRequest -Uri 'http://<Your SCOM MS>/OperationsManager/data/UnsealedManagementPacks' -Method Get -WebSession $WebSession
+
+# Convert our response from JSON format to a custom object or hash table
+$UnsealedMPs = ConvertFrom-Json -InputObject $Response.Content
+
+# Print out a list of all unsealed management packs
+$UnsealedMPs
