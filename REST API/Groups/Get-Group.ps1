ccc17c889ef56b899d85d455555bf3107604c441
Leon Laude <44682947+LeonLaude@users.noreply.github.com>
2020-07-10 17:34:28 +0200

    Update Get-Group.ps1

--- REST API/Groups/Get-Group.ps1
+++ REST API/Groups/Get-Group.ps1
@@ -9,12 +9,12 @@ $JSONBody = $EncodedText | ConvertTo-Json
 # The SCOM REST API authentication URL
 $UriBase = 'http://<Your SCOM MS>/OperationsManager/authenticate'
 
-# Authentication
-$Authentication = Invoke-RestMethod -Method Post -Uri $UriBase -Headers $SCOMHeaders -Body $JSONBody -UseDefaultCredentials -SessionVariable WebSession
-
 # Initiate the Cross-Site Request Forgery (CSRF) token, this is to prevent CSRF attacks
 $CSRFtoken = $WebSession.Cookies.GetCookies($UriBase) | ? { $_.Name -eq 'SCOM-CSRF-TOKEN' }
 $SCOMHeaders.Add('SCOM-CSRF-TOKEN', [System.Web.HttpUtility]::UrlDecode($CSRFtoken.Value))
+
+# Authentication
+$Authentication = Invoke-RestMethod -Method Post -Uri $UriBase -Headers $SCOMHeaders -Body $JSONBody -UseDefaultCredentials -SessionVariable WebSession
 
 # Criteria: Enter part of the displayname of the SCOM group within the wildcards
 $Criteria = "DisplayName LIKE '%Contoso%'"
