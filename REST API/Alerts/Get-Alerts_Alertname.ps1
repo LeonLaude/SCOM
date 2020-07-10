5d15b8e1da0bf21b4042c77958c051978e24e3f0
Leon Laude <44682947+LeonLaude@users.noreply.github.com>
2020-07-10 17:33:25 +0200

    Update Get-Alerts_Alertname.ps1

--- REST API/Alerts/Get-Alerts_Alertname.ps1
+++ REST API/Alerts/Get-Alerts_Alertname.ps1
@@ -9,12 +9,12 @@ $JSONBody = $EncodedText | ConvertTo-Json
 # The SCOM REST API authentication URL
 $URIBase = 'http://<Your SCOM MS>/OperationsManager/authenticate'
 
-# Authentication
-$Authentication = Invoke-RestMethod -Method POST -Uri $uriBase -Headers $SCOMHeaders -body $JSONBody -UseDefaultCredentials -SessionVariable WebSession
-
 # Initiate the Cross-Site Request Forgery (CSRF) token, this is to prevent CSRF attacks
 $CSRFtoken = $WebSession.Cookies.GetCookies($UriBase) | ? { $_.Name -eq 'SCOM-CSRF-TOKEN' }
 $SCOMHeaders.Add('SCOM-CSRF-TOKEN', [System.Web.HttpUtility]::UrlDecode($CSRFtoken.Value))
+
+# Authentication
+$Authentication = Invoke-RestMethod -Method POST -Uri $uriBase -Headers $SCOMHeaders -body $JSONBody -UseDefaultCredentials -SessionVariable WebSession
 
 # The query which contains the criteria for our alerts
 $Query = @(@{ "classId"= ""
